7c38ccd271177356535086f8f7b7a727bde455d1
Joshua Shanks <jjshanks@gmail.com>
2020-02-23 07:50:45 -0800

    Add ssh key

--- prod/main.tf
+++ prod/main.tf
@@ -1,6 +1,7 @@
 terraform {
   required_providers {
     digitalocean = "= 1.14"
+    tls = "= 2.1.1"
   }
 }
 
@@ -16,4 +17,14 @@ resource "digitalocean_project" "prod" {
 
 resource "digitalocean_domain" "prod" {
   name = "sampleinfra.com"
+}
+
+resource "tls_private_key" "prod" {
+  algorithm   = "RSA"
+  rsa_bits = "4096"
+}
+
+resource "digitalocean_ssh_key" "prod" {
+  name       = "Sample Infra Prod (TF Managed)"
+  public_key = tls_private_key.prod.public_key_openssh
 }
