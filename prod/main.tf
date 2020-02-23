ea06d50af75b0a2fe719f82e88a7ffc2ebd35851
Joshua Shanks <jjshanks@gmail.com>
2020-02-22 20:40:02 -0800

    Add domain

--- prod/main.tf
+++ prod/main.tf
@@ -11,4 +11,9 @@ resource "digitalocean_project" "prod" {
   description = "Production Project for Sample Infra"
   purpose     = "Sample Infra"
   environment = "Production"
+  resources   = [digitalocean_domain.prod.urn]
+}
+
+resource "digitalocean_domain" "prod" {
+  name = "sampleinfra.com"
 }
