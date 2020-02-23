39b9b41dbcefdbaaee99fd2666dd86a5b9cde6b1
Joshua Shanks <jjshanks@gmail.com>
2020-02-23 09:13:15 -0800

    Add firewall for docker droplet

--- prod/main.tf
+++ prod/main.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     digitalocean = "= 1.14"
     tls          = "= 2.1.1"
+    http         = "= 1.1.1"
   }
 }
 
@@ -27,4 +28,58 @@ resource "tls_private_key" "prod" {
 resource "digitalocean_ssh_key" "prod" {
   name       = "Sample Infra Prod (TF Managed)"
   public_key = tls_private_key.prod.public_key_openssh
+}
+
+data "http" "icanhazip" {
+  url = "http://icanhazip.com"
+}
+
+resource "digitalocean_firewall" "web" {
+  name        = "only-22-80-and-443"
+  tags        = ["tf"]
+  droplet_ids = [digitalocean_droplet.docker01.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["${chomp(data.http.icanhazip.body)}/24"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+resource "digitalocean_droplet" "docker01" {
+  image    = "docker-18-04"
+  name     = "docker-01"
+  region   = var.do_region
+  size     = "s-1vcpu-1gb"
+  ssh_keys = [digitalocean_ssh_key.prod.id]
+  tags     = ["tf"]
 }
---
+++ prod/output.tf
@@ -0,0 +1,3 @@
+output "docker01_ip" {
+  value = digitalocean_droplet.docker01.ipv4_address
+}
