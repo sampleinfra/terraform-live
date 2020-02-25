f44c9dc7a1db9304d6b5cfbcb0f7fac060935b4c
Joshua Shanks <jjshanks@gmail.com>
2020-02-24 19:19:26 -0800

    fmt

--- prod/echo/main.tf
+++ prod/echo/main.tf
@@ -98,5 +98,5 @@ resource "digitalocean_record" "echo" {
   domain = data.terraform_remote_state.shared.outputs.domain_name
   type   = "A"
   name   = "echo"
-  value  =  digitalocean_droplet.docker01.ipv4_address
+  value  = digitalocean_droplet.docker01.ipv4_address
 }
